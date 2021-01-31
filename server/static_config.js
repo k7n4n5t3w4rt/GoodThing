6923df4aa6f31d35099f256fd4b167ea77f640d2
Kynan Stewart Hughes <mail@kynan.net.au>
2021-01-31 14:00:37 +1100

    Simplified the generate script/logic by removing cache option

--- server/static_config.js
+++ server/static_config.js
@@ -12,7 +12,8 @@ export const appPaths = () /*: Array<string> */ => {
   if (Array.isArray(keyValues)) {
     keyValues.forEach((currentElement /*: string */) /*: void */ => {
       const [key, value] = currentElement.split("=");
-      paths.push(value.replace(/\"/g, ""));
+      const valueNoParams = value.split("/:")[0];
+      paths.push(valueNoParams.replace(/\"/g, ""));
     });
   }
   return paths;
