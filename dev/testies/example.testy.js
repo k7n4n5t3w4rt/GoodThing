84c6da59f30581ce6dba519038c48fed5ad08917
Kynan Stewart Hughes <mail@kynan.net.au>
2024-07-24 17:40:55 +1000

    v2.0.8: Fixed up Flow errors in the Testy runner.

--- dev/testies/example.testy.js
+++ dev/testies/example.testy.js
@@ -1,6 +1,5 @@
 // @flow
 import { test, testPromise, should } from "../testy.js";
-import fs, { read } from "fs";
 
 test("Nothing | Just a simple test example", () /*: any */ => {
   const aString /*: string */ = "This is a testy test test.";
